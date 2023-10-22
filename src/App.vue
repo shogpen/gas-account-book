7497dd71e1bc888bf9975e6c89f0a63774bdb543
shogpen <49538911+shogpen@users.noreply.github.com>
2023-10-23 07:50:19 +0900

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     <!-- ツールバー -->
     <v-app-bar app color="green" dark>
       <!-- タイトル -->
-      <v-toolbar-title>{{ appName }}</v-toolbar-title>
+      <v-toolbar-title>{{ appName }} + da</v-toolbar-title>
       <v-spacer></v-spacer>
       <!-- テーブルアイコンのボタン -->
       <v-btn icon to="/">
